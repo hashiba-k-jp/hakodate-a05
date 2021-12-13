1bb46f33eeffbcf79323989471c160b9e8c818b6
ksatoshi <ksatoshi@mail.ksatoshi.dev>
2021-12-13 22:51:11 +0900

    funcs.pyで動作するように変更、デバッグモード時にSQL等を出力しないよう変更

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ from linebot import(
 )
 import linebot
 
+from funcs import *
+
 app = Flask(__name__)
 
 #各種定数を定義
@@ -67,24 +69,18 @@ def control_form():
         if user_accept == '1':
             #DBからuserのidを取得
             sql = "SELECT user_id FROM public.verify WHERE id='{}';".format(user_uuid)
-            if DEBUG == True:
-                print('SQL EXECUTE:{}'.format(sql))
             cursor.execute(sql)
             user_id = cursor.fetchone()[0]
             conn.commit()
 
             #DBから函館の地域コードを取得
             sql = "SELECT id FROM public.area WHERE area_name='函館市';"
-            if DEBUG == True:
-                print('SQL EXECUTE:{}'.format(sql))
             cursor.execute(sql)
             area_id = cursor.fetchone()[0]
             conn.commit()
 
             #resistrationにすでに登録されているかを確認
             sql = 'SELECT EXISTS (SELECT * FROM public.resistration WHERE user_id={user_id} AND area_id={area_id});'.format(user_id=user_id,area_id=area_id)
-            if DEBUG == True:
-                print('SQL EXECUTE:{}'.format(sql))
             cursor.execute(sql)
             resistration_result = cursor.fetchone()[0]
             conn.commit()
@@ -105,15 +101,11 @@ def control_form():
                     user_id = user_id,
                     area_id = area_id
                 )
-                if DEBUG == True:
-                    print('SQL EXECUTE:{}'.format(sql))
                 cursor.execute(sql)
                 conn.commit()
 
                 #verifyからユーザーを削除
                 sql = "DELETE FROM public.verify WHERE id = '{}';".format(user_uuid)
-                if DEBUG == True:
-                    print('SQL EXECUTE:{}'.format(sql))
                 cursor.execute(sql)
                 conn.commit()
 
@@ -131,16 +123,12 @@ def control_form():
             
             #userのidを取得
             sql = "SELECT user_id FROM public.verify WHERE id='{}'".format(user_uuid)
-            if DEBUG == True:
-                print('SQL EXECUTE:{}'.format(sql))
             cursor.execute(sql)
             id = cursor.fetchone()[0]
             conn.commit()
             
             #public.userからユーザ情報を削除
             sql = 'DELETE FROM public.user WHERE id={}'.format(id)
-            if DEBUG == True:
-                print('SQL EXECUTE:{}'.format(sql))
             cursor.execute(sql)
             conn.commit()
               
@@ -176,16 +164,12 @@ def webhock():
     conn = db_connect()
 
     if validation(body=body, signature=signature.encode('utf-8')) == True: #イベントの真贋判定
-        if DEBUG == True:
-            print('This is regular request!!')
         
         for line in data["events"]:
             user_id=''
             #ソースがユーザからのイベントである場合のみuser_idを抽出
             if line['source']['type'] == 'user':
                 user_id = line["source"]['userId']
-                if DEBUG == True:
-                    print('user_id:{}'.format(user_id))
             else:
                 #ソースがユーザーからのイベントではない時400を返して処理を終える
                 return '',200,{}
@@ -206,8 +190,6 @@ def webhock():
             #存在しない時DBに登録
             if result[0] == False:
                 sql = "INSERT INTO public.user(user_id) VALUES('{}');".format(user_id)
-                if DEBUG == True:
-                    print('SQL EXECUTE:{}'.format(sql))
                 cursor.execute(sql)
                 conn.commit()
 
@@ -219,23 +201,17 @@ def webhock():
 
                     #DBからuserのidを取得
                     sql = "SELECT id FROM public.user WHERE user_id='{}'".format(user_id)
-                    if DEBUG == True:
-                        print('EXECUTE SQL:{}'.format(sql))
                     cursor.execute(sql)
                     id = cursor.fetchone()[0]
                     conn.commit()
                     
                     #public.verifyにユーザーの情報が存在する場合は削除する
                     sql ="SELECT EXISTS (SELECT * FROM public.verify WHERE user_id={});".format(id)
-                    if DEBUG == True:
-                        print('SQL EXECUTE:{}'.format(sql))
                     cursor.execute(sql)
                     
                     if cursor.fetchone()[0] == True:
                         sql = "DELETE FROM public.verify WHERE user_id={}".format(id)
 
-                        if DEBUG == True:
-                            print('SQL EXECUTE:{}'.format(sql))
                         cursor.execute(sql)
                         conn.commit()
                     else:
@@ -315,25 +291,6 @@ def get_location_post():
 
         return "completed!"
 
-#LINEユーザにメッセージを送信する関数
-def send_msg_with_line(user_id,msgs):
-    send_msg = TextSendMessage(text='')
-    try:
-        line_bot_api = LineBotApi(ACCESS_TOKEN)
-
-        for msg in msgs:
-            if DEBUG == True:
-                print('SENDING MESSAGE:{}'.format(msg))
-            send_msg = TextSendMessage(text=msg)
-            line_bot_api.push_message(user_id,send_msg)
-    except linebot.exceptions.LineBotApiError as e:
-        print(e.error.message)
-        print(e.error.details)
-
-    for msg in msgs:
-        if DEBUG == True:
-            print('SENNDING MESSAGE:{}'.format(msg))
-
 #署名検証用の関数
 def validation(body,signature):
     hash = hmac.new(CHANNEL_SECRET.encode('utf-8'),
@@ -348,24 +305,6 @@ def validation(body,signature):
         return True
     else:
         return False
-
-#DB接続用の関数
-def db_connect():
-    #環境変数からデータベースの情報を取得
-    DATABASE_URL = os.environ.get('DATABASE_URL')
-
-    #接続先文字列の生成
-    connection_info = DATABASE_URL
-
-    print('Connecting:{info}'.format(info=connection_info))
-    conn = ''
-    try:
-        conn = psycopg2.connect(connection_info)
-    except psycopg2.Error: 
-        print('Database connection failed!!') #DBとの接続に失敗した場合は終了する
-        sys.exit()
-
-    return conn
 
 if __name__ == "__main__":
     initApp()
--- funcs.py
+++ funcs.py
@@ -8,6 +8,10 @@ from linebot import(
 )
 import linebot
 
+#各種定数を環境変数から取得
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
 #LINEユーザにメッセージを送信する関数
 def send_msg_with_line(user_id,msgs):
     send_msg = TextSendMessage(text='')
@@ -15,31 +19,19 @@ def send_msg_with_line(user_id,msgs):
         line_bot_api = LineBotApi(ACCESS_TOKEN)
 
         for msg in msgs:
-            if DEBUG == True:
-                print('SENDING MESSAGE:{}'.format(msg))
             send_msg = TextSendMessage(text=msg)
             line_bot_api.push_message(user_id,send_msg)
     except linebot.exceptions.LineBotApiError as e:
         print(e.error.message)
         print(e.error.details)
 
-    for msg in msgs:
-        if DEBUG == True:
-            print('SENNDING MESSAGE:{}'.format(msg))
-
 #DB接続用の関数
 def db_connect():
-    #環境変数からデータベースの情報を取得
-    DATABASE_URL = os.environ.get('DATABASE_URL')
-    print(DATABASE_URL)
 
-    #接続先文字列の生成
-    connection_info = DATABASE_URL
-
-    print('Connecting:{info}'.format(info=connection_info))
+    print('Connecting:DataBase')
     conn = ''
     try:
-        conn = psycopg2.connect(connection_info)
+        conn = psycopg2.connect(DATABASE_URL)
     except psycopg2.Error:
         print('Database connection failed!!') #DBとの接続に失敗した場合は終了する
         sys.exit()
