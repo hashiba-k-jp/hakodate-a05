17ef4e9d012db580fbd93d1f23e63d5e0c0a6555
hashiba-k-jp <17132@hakodate.kosen-ac.jp>
2021-12-13 17:03:33 +0900

    functions file added!
    some functions copied to the other file.

---
+++ funcs.py
@@ -0,0 +1,47 @@
+# funcs stands for FUNCtionS
+
+import os, sys
+import psycopg2
+from linebot.models import TextSendMessage
+from linebot import(
+    LineBotApi
+)
+import linebot
+
+#LINEユーザにメッセージを送信する関数
+def send_msg_with_line(user_id,msgs):
+    send_msg = TextSendMessage(text='')
+    try:
+        line_bot_api = LineBotApi(ACCESS_TOKEN)
+
+        for msg in msgs:
+            if DEBUG == True:
+                print('SENDING MESSAGE:{}'.format(msg))
+            send_msg = TextSendMessage(text=msg)
+            line_bot_api.push_message(user_id,send_msg)
+    except linebot.exceptions.LineBotApiError as e:
+        print(e.error.message)
+        print(e.error.details)
+
+    for msg in msgs:
+        if DEBUG == True:
+            print('SENNDING MESSAGE:{}'.format(msg))
+
+#DB接続用の関数
+def db_connect():
+    #環境変数からデータベースの情報を取得
+    DATABASE_URL = os.environ.get('DATABASE_URL')
+    print(DATABASE_URL)
+
+    #接続先文字列の生成
+    connection_info = DATABASE_URL
+
+    print('Connecting:{info}'.format(info=connection_info))
+    conn = ''
+    try:
+        conn = psycopg2.connect(connection_info)
+    except psycopg2.Error:
+        print('Database connection failed!!') #DBとの接続に失敗した場合は終了する
+        sys.exit()
+
+    return conn
