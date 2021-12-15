0b098885df7c0576de5fc52d761d3f91f911c2b1
hashiba-k-jp <17132@hakodate.kosen-ac.jp>
2021-12-15 13:18:39 +0900

    Update getInfo.py
    test turned OFF

--- src/getInfoFromJMA/getInfo.py
+++ src/getInfoFromJMA/getInfo.py
@@ -15,7 +15,7 @@ import psycopg2
 
 def getInfo():
     print('CALLED \'src/getInfoFromJMA/getInfo.py\'')
-    isTest = True # add hakodate city and warningCode=20
+    isTest = False # add hakodate city and warningCode=20
 
     # this function dose ...
     # ~ get information from JAM server
